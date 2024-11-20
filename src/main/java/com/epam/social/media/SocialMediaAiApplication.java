96cb2401daa4e9cf6b9f4f8ea0c69a1b8242cbb4
TAlextu <talextu@gmail.com>
2024-11-20 16:35:07 +0200

    initial commit

---
+++ src/main/java/com/epam/social/media/SocialMediaAiApplication.java
@@ -0,0 +1,13 @@
+package com.epam.social.media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialMediaAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/social/media/SocialMediaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.social.media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
